65d4556419c467ccf891b39553e67dabea25005f
portatil-pisco-daniel <danielcuellarp@gmail.com>
2021-02-06 11:43:59 -0500

    agregando morgan

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path') 
+const app = express()
 
 // Configuracion
-app.set('port', 4000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// Rutas
-app.use(require('./routes'));
-
-// Static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', 4000) 
+app.set('views', path.join(__dirname, 'views')) // ubicacion de las vistas
+app.set('view engine', 'ejs') // motor de plantillas
+app.use(morgan('dev')) // logger de morgan
+app.use(require('./routes')) // ubicacion de las rutas
+app.use(express.static(path.join(__dirname, 'public'))) // set carpeta publica
 
 // Servidor
 app.listen(app.get('port'), () => {
-  console.log("Servidor ejecutandose", app.get('port'))
+  console.log("Servidor ejecutandose", app.get('port'));
 })
+
