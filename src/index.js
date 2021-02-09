4ade8eba3d41d4332996c44b3759b334ed45a72b
portatil-pisco-daniel <danielcuellarp@gmail.com>
2021-02-08 20:06:04 -0500

    Login: registro de usuario, conexion a MongoDB

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const morgan = require('morgan')
+const passport = require('passport')
 const path = require('path') 
+const session = require('express-session')
+const flash = require('connect-flash')
+
 const app = express()
+require('./database')
+require('./passport/local-auth')
 
 // Configuracion
 app.set('port', process.env.PORT || 4000) 
 app.set('views', path.join(__dirname, 'views')) // ubicacion de las vistas
 app.set('view engine', 'ejs') // motor de plantillas
 app.use(morgan('dev')) // logger de morgan
+app.use(express.urlencoded({extended: false}))
+app.use(session({
+  secret: 'ClaveSecreta',
+  resave: false,
+  saveUninitialized: false
+})) // parametrizar la session
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use((req, res, next) => {
+  app.locals.signupMessage = req.flash('signupMessage')
+  next()
+})
+
 app.use(require('./routes')) // ubicacion de las rutas
 app.use(express.static(path.join(__dirname, 'public'))) // set carpeta publica
+
 
 // Servidor
 app.listen(app.get('port'), () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
+const datos = { 
+  title: 'Daniel Cuéllar P', 
+  anio: new Date().getFullYear()
+}
 
 router.get('/', (req, res) => {
-  res.render('index', {title: 'Daniel Cuéllar P', anio: new Date().getFullYear()});
+  res.render('index', datos);
 })
 
 router.get('/contact', (req, res) => {
-  res.render('contact',{title: 'Daniel Cuéllar P - Contact', anio: new Date().getFullYear()});
+  res.render('contact',datos);
+})
+
+router.get('/app', (req, res, next) => {
+  res.render('indexApp',datos);
+})
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup',datos);
+})
+
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/app',
+  failureRedirect: '/signup',
+  passReqToCallback: true
+}))
+
+router.get('/signin', (req, res, next) => {
+
+})
+
+router.post('/signin', (req, res, next) => {
+  
 })
 
 module.exports = router;
