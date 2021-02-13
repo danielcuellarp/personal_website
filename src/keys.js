bcce9084974ecd504d4711da41d675ec91fa6f8d
portatil-pisco-daniel <danielcuellarp@gmail.com>
2021-02-13 17:07:53 -0500

    verificando rama principal

---
+++ src/keys.js
@@ -0,0 +1,6 @@
+// archivo con las keys de la base de datos
+module.exports = {
+  mongodb: {
+    uri: "mongodb+srv://dbUser:halaMadrid10@cluster0.phyq7.mongodb.net/app?retryWrites=true&w=majority"
+  }
+}
