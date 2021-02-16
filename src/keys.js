34d3998c6dc6a1e473dc1e91193dc855ee4cc0de
portatil-pisco-daniel <danielcuellarp@gmail.com>
2021-02-15 20:40:50 -0500

    Eliminando archivos no deseados

--- src/keys.js
+++
@@ -1,6 +0,0 @@
-// archivo con las keys de la base de datos
-module.exports = {
-  mongodb: {
-    uri: "mongodb+srv://dbUser:halaMadrid10@cluster0.phyq7.mongodb.net/app?retryWrites=true&w=majority"
-  }
-}
